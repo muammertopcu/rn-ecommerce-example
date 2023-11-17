7a0173483c3d6a23ddd4e1fa283d79a721372da8
muammertopcu <muammertop@outlook.com>
2023-11-17 10:06:33 +0300

    implemented native-base

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React, {ReactElement} from 'react';
+import {NativeBaseProvider} from 'native-base';
+
+function App(): ReactElement {
+  return <NativeBaseProvider />;
+}
+
+export default App;
